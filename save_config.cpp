55293ba2831cd98f30cedd8e5946e00fd3f11704
BaNavyBlue <abajor@ucsc.edu>
2023-12-22 19:50:40 -0800

    adding json file generation stuff

---
+++ save_config.cpp
@@ -0,0 +1,12 @@
+#include "save_config.h"
+
+const char ws[5] = "    ";
+
+bool save_config(std::vector<BUTTON> saveButtons, std::string fileName)
+{
+    std::string outTxt;
+    for(unsigned i = 0; i < saveButtons.size(); ++i){
+        
+    }
+    return true;
+}
---
+++ save_config.h
@@ -0,0 +1,9 @@
+#ifndef SAVE_CONFIG_H
+#define SAVE_CONFIG_H
+#include "My_Headers.h"
+#include "button.h"
+
+
+bool save_config(std::vector<BUTTON> saveButtons, std::string fileName);
+
+#endif // SAVE_CONFIG_H
